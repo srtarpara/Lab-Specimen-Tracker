3c16da8defa72b2cbed03ef1ee72fee27fcc63c5
Shubhm <srtarpara05@gmail.com>
2026-03-17 18:16:50 -0400

    add specimens API endpoints

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 3/16/2026 - Created the database in postgres sql. You can find the database schema in the database.sql file.
 3/16/2026 - Created the backend setup, database.py builds the connection to the Postgres SQL server, and main.py creates the root endpoint and the patients endpoint that pulls all the patient data from the table in the database and returns it. Had an issue with the password not matching in the .env folder and the database.py when it builds the connection. Was able to reset the password in Postgres SQL to match and enabled the whole code to work perfectly.
+3/17/2026 - Created the specimen API endpoints, specifically the get all specimens, get a certain specimen, and inject a new specimen into the database.
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from database import get_connection
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -17,3 +18,60 @@ def get_patients():
     conn.close()
 
     return {"patients": rows}
+
+@app.get("/specimens")
+def get_specimens():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+                   SELECT s.id, p.name as patient_name, s.type, s.status, s.collected_at
+                   FROM specimens s
+                   JOIN patients p ON s.patient_id = p.id
+                """)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return {"specimens": rows}
+
+@app.get("/specimens/{specimen_id}")
+def get_specific_specimen(specimen_id: int):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+                    SELECT s.id, p.name as patient_name, s.type, s.status, s.collected_at
+                   FROM specimens s
+                   JOIN patients p ON s.patient_id = p.id
+                   WHERE s.id = %s
+                   """, (specimen_id,))
+    row = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    if row is None:
+        return {"error": "Specimen not found"}
+
+    return {"specimen": row}
+
+class SpecimenCreate(BaseModel):
+    patient_id: int
+    type: str
+    collected_by: int
+
+@app.post("/specimens")
+def create_specimen(specimen: SpecimenCreate):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+                   INSERT INTO specimens (patient_id, type, collected_by, status)
+                   VALUES (%s, %s, %s, 'Collected')
+                   RETURNING id
+                """, (specimen.patient_id, specimen.type, specimen.collected_by))
+    new_id = cursor.fetchone()[0]
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return {"message": "Specimen created", "id": new_id}
+    
+    
