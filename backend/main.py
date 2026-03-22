b0846b319f9c43a7581603bfd29d618cbb592590
Shubhm <srtarpara05@gmail.com>
2026-03-21 21:07:37 -0400

    fix CORS for Vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
+    allow_origins = ["http://localhost:3000",
+                     "https://lab-specimen-tracker.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
